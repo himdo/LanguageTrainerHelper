eed8f207c21da572f3c4b07579d85a5b1f6410d7
lennon <hitzemanl@yahoo.com>
2022-09-28 17:29:47 -0700

    Added a quick hack to get Speech to Text working. I want to revisit this to get the model self contained and to change the language on demand

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,6 +1,28 @@
+import os
+
 class SpeechToText:
+    # This is using whisper as the main model
+    # For this to work you need to setup and install whisper to your system, see https://github.com/openai/whisper#setup
+
     def __init__(self):
-        pass
+        self.lang = 'English'
     
+    # This returns a string or False if something went wrong
     def GenerateText(self, pathToFile):
-        pass
+        try:
+            os.system('whisper ' + str(pathToFile) + ' --language ' + self.lang)
+            textPath = str(pathToFile) + '.txt'
+            f = open(textPath)
+            fileContents = ""
+            for x in f:
+                fileContents += x
+            f.close()
+            # Cleanup whisper files
+            os.remove(textPath)
+            os.remove(str(pathToFile) + '.vtt')
+            return x
+        except:
+            return False
+
+# stt = SpeechToText()
+# print(stt.GenerateText('./jfk.flac'))
